9158f311e828552bab9349560f8c6b9518494bfb
hagd <hagd0520@gmail.com>
2023-11-14 10:55:36 +0900

    calc3~5

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.sb20231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -33,5 +34,35 @@ public class HomeController {
     @ResponseBody
     String showCalc2(Integer a, Integer b) {
         return "a : " + a + ", b : " + b;
+    }
+
+    @GetMapping("/calc3")
+    @ResponseBody
+    String showCalc3(
+            // url의 내용은 무조건 문자열이기 때문에 defaultValue는 무조건 문자로 넣어야한다.
+            @RequestParam(defaultValue = "0") int a,
+            @RequestParam(defaultValue = "0") int b
+    ) {
+        return "계산 결과 : %d".formatted(a + b) + " a : " + a + ", b : " + b;
+    }
+
+    @GetMapping("/calc4")
+    @ResponseBody
+    String showCalc4(
+            //
+            @RequestParam(defaultValue = "0") double a,
+            @RequestParam(defaultValue = "0") double b
+    ) {
+        return "계산 결과 : %f".formatted(a + b) + " a : " + a + ", b : " + b;
+    }
+
+    @GetMapping("/calc4")
+    @ResponseBody
+    String showCalc4(
+            //
+            @RequestParam(defaultValue = "0") String a,
+            @RequestParam(defaultValue = "0") String b
+    ) {
+        return "계산 결과 : %f".formatted(a + b) + " a : " + a + ", b : " + b;
     }
 }
